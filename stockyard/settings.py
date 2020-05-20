5d64fcdfa0c99b1f756043ef8123f15133b4cdac
sid24gupta <47803520+sid24gupta@users.noreply.github.com>
2020-05-20 13:29:05 +0530

    Update settings.py

--- stockyard/settings.py
+++ stockyard/settings.py
@@ -123,9 +123,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 
